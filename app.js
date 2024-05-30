bd6fbe9deb7d72e8dd47599119f8b120cfcbef17
Valerie_Achkan <valery.achkan@gmail.com>
2024-05-30 15:13:39 +0300

    Adds env

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const contactsRouter = require('./routes/contactsRouter.js');
 
 const app = express();
 
-const { DB_HOST } = process.env;
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000);
-    console.log('Database connection successful');
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+app.use('/api/contacts', contactsRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message });
+});
+
+// app.listen(3000, () => {
+//   console.log('Server is running. Use our API on port: 3000');
+// });
+module.exports = app;
